648dc71992fa7afe4aef86ff49f3e113f20d4e21
Wendi Chen <epanistamai.proles@gmail.com>
2018-11-24 05:56:44 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,36 @@
 # Write your code here.
+
+def badge_maker(name)
+    return "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(name_array)
+  list = []
+  name_array.each do |name|
+    list << badge_maker(name)
+  end
+  list
+end
+
+def assign_rooms(attendees)
+   rooms = 1
+   room_assignments = []
+    attendees.each do |name|
+       room_assignments << "Hello, #{name}! You'll be assigned to room #{rooms}!"
+       rooms += 1
+    end
+  room_assignments
+end
+
+def printer(attendees)
+      badge_and_room_assignments = []
+      badge_and_room_assignments << batch_badge_creator(attendees)
+        badge_and_room_assignments.each do |name|
+           puts name.join(' ')  
+    end
+      badge_and_room_assignments << assign_rooms(attendees)
+        badge_and_room_assignments.each do |room|
+          puts room.join(' ')
+    end
+  
+end    
